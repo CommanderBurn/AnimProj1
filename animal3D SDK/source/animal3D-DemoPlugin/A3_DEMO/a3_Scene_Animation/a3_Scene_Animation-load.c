64ea3054a1e58abd214400c11093c1f8857f9556
CommanderBurn <commandrburn@gmail.com>
2025-10-31 08:45:23 -0400

    Changes

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Scene_Animation/a3_Scene_Animation-load.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Scene_Animation/a3_Scene_Animation-load.c
@@ -709,6 +709,7 @@ void a3animation_init_animation(a3_DemoState const* demoState, a3_Scene_Animatio
 	a3hierarchySetNode(scene->blendTree, 3, 1, "blendTree_idle_m");
 	a3hierarchySetNode(scene->blendTree, 4, 0, "blendTree_idle_p");
 	
+	//a3spatialPoseBlendTreeCreate(scene->blendTree, hierarchy);
 	// blend tree states
 	for (p = 0, j = scene->blendTree->numNodes; p < j; ++p)
 	{
@@ -745,22 +746,6 @@ void a3animation_init_animation(a3_DemoState const* demoState, a3_Scene_Animatio
 		}
 	}
 
-	a3_SpatialPoseBlendTree* tree;
-	tree = (a3_SpatialPoseBlendTree*)malloc(hierarchyState->hierarchy->numNodes * sizeof(a3_SpatialPoseBlendTree));
-	tree->nodes = (a3_SpatialPoseBlendNode*)malloc(hierarchyState->hierarchy->numNodes * sizeof(a3_SpatialPoseBlendNode));
-	a3real half = 0.5f;
-	for (a3ui32 i = 0; i < hierarchyState->hierarchy->numNodes; ++i)
-	{
-		tree->nodes[i].blendOpSet = scene->blendOpLERP;
-		tree->nodes[i].pose_ctrl[0] = hierarchyState->animPose[i].hpose_base;
-		if(i != 0)
-			tree->nodes[i].pose_ctrl[1] = hierarchyState->animPose[i+1].hpose_base;
-		tree->nodes[i].pose_out = hierarchyState->animPose[i].hpose_base;
-		tree->nodes[i].u[0] = &half;
-		tree->nodes[i].uCount = hierarchyState->hierarchy->numNodes;
-		tree->nodes[i].vCount = hierarchyState->hierarchy->numNodes;
-	}
-	a3spatialPoseBlendTreeCreate(tree, hierarchyState->hierarchy);
 }
 
 
